449d61309e410e5d46565333cc8d3a8979a664b3
Roy Tam <roytam@gmail.com>
2019-03-25 21:32:14 +0800

    import changes from tenfourfox: - #334: remove tele from MSE (9279f48ca) - #334: remove even moar BrowserGlue tele (64890af9b) - chemspill: M1537924 M1538006 (backported to ESR45) (b22dfbd77)

--- dom/html/HTMLMediaElement.cpp
+++ dom/html/HTMLMediaElement.cpp
@@ -2651,6 +2651,7 @@ HTMLMediaElement::ReportEMETelemetry()
 void
 HTMLMediaElement::ReportMSETelemetry()
 {
+#if(0)
   // Report telemetry for videos when a page is unloaded. We
   // want to know data on what state the video is at when
   // the user has exited.
@@ -2703,6 +2704,7 @@ HTMLMediaElement::ReportMSETelemetry()
   Telemetry::Accumulate(Telemetry::VIDEO_MSE_JOIN_LATENCY_MS, SECONDS_TO_MS(latency));
   LOG(LogLevel::Debug, ("%p VIDEO_MSE_JOIN_LATENCY = %f (%d ms) count=%d\n",
                      this, latency, SECONDS_TO_MS(latency), mJoinLatency.Count()));
+#endif
 }
 
 void HTMLMediaElement::UnbindFromTree(bool aDeep,
@@ -4239,12 +4241,14 @@ void HTMLMediaElement::SuspendOrResumeElement(bool aPauseElement, bool aSuspendE
     UpdateSrcMediaStreamPlaying();
     UpdateAudioChannelPlayingState();
     if (aPauseElement) {
+#if(0)
       if (mMediaSource) {
         ReportMSETelemetry();
 #ifdef MOZ_EME
         ReportEMETelemetry();
 #endif
       }
+#endif
 
 #ifdef MOZ_EME
       // For EME content, force destruction of the CDM client (and CDM
