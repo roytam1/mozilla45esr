656ce608c4d0c1b99b6005cb49539805f9184446
roytam1 <roytam@gmail.com>
2020-10-23 14:41:37 +0800

    import changes from tenfourfox: - OlgaTPark/tenfourfox#14 — M1144749, M1344211, M1441788 — Add a keyboard shortcut for Reader Mode (61f16fc35) - #629: fix locale, add also to menus (81c8a93b9)

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -5382,6 +5382,7 @@ const nodeToTooltipMap = {
   "tabs-newtab-button": "newTabButton.tooltip",
   "fullscreen-button": "fullscreenButton.tooltip",
   "downloads-button": "downloads.tooltip",
+  "reader-mode-button": "reader-mode-button.tooltip",
 };
 const nodeToShortcutMap = {
   "bookmarks-menu-button": "manBookmarkKb",
@@ -5389,7 +5390,8 @@ const nodeToShortcutMap = {
   "new-tab-button": "key_newNavigatorTab",
   "tabs-newtab-button": "key_newNavigatorTab",
   "fullscreen-button": "key_fullScreen",
-  "downloads-button": "key_openDownloads"
+  "downloads-button": "key_openDownloads",
+  "reader-mode-button": "key_toggleReaderMode",
 };
 
 if (AppConstants.platform == "macosx") {
