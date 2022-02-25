b554fc902734dbc7e0d069cc2cf9b1880332a500
roytam1 <roytam@gmail.com>
2022-02-25 11:31:59 +0800

    partly import changes from tenfourfox: - #654: M1746720 M1737816 M1746011 M1739957 M1740985+backbugs M1742421, update HSTS, TLDs (861ae8f62) - #654: lozad and yt workarounds (5e969c18d)

--- widget/cocoa/nsCocoaWindow.mm
+++ widget/cocoa/nsCocoaWindow.mm
@@ -1230,6 +1230,8 @@ NS_METHOD nsCocoaWindow::SetSizeMode(nsSizeMode aMode)
       [mWindow deminiaturize:nil];
     else if (previousMode == nsSizeMode_Maximized && [mWindow isZoomed])
       [mWindow zoom:nil];
+    else if (previousMode == nsSizeMode_Fullscreen)
+      MakeFullScreen(false);
   }
   else if (aMode == nsSizeMode_Minimized) {
     if (![mWindow isMiniaturized])
